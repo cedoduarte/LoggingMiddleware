083216a470b2f47e245101d3cccdb2360f39305a
Carlos Duarte <carlosduarte.1@hotmail.com>
2024-08-07 17:15:19 -0700

    first

---
+++ WebApplication1/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : Controller
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpGet("person")]
+        public async Task<IActionResult> GetPersonById(int id)
+        {
+            return Ok(await _personService.GetPersonById(id));
+        }
+
+        [HttpGet("persons")]
+        public async Task<IActionResult> GetPersons()
+        {
+            return Ok(await _personService.GetPersons());
+        }
+    }
+}
---
+++ WebApplication1/Services/PersonService.cs
@@ -0,0 +1,55 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Services
+{
+    public interface IPersonService
+    {
+        Task<Person?> GetPersonById(int id);
+        Task<List<Person>> GetPersons();
+    }
+
+    public class PersonService : IPersonService
+    {
+        private List<Person> _persons = new() 
+        {
+            new Person()
+            {
+                Id=  1,
+                FirstName = "Carlos",
+                LastName = "Duarte",
+                Age = 29
+            },
+            new Person()
+            {
+                Id= 2,
+                FirstName = "Ana",
+                LastName = "Lopez",
+                Age = 29
+            },
+            new Person()
+            {
+                Id= 3,
+                FirstName = "Luis",
+                LastName = "Rodriguez",
+                Age = 29
+            }
+        };
+
+        public async Task<Person?> GetPersonById(int id)
+        {
+            foreach (Person person in _persons)
+            {
+                if (person.Id == id)
+                {
+                    return person;
+                }
+            }
+            return null;
+        }
+
+        public async Task<List<Person>> GetPersons()
+        {
+            return _persons;
+        }
+    }
+}
